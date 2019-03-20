c8fc50a7914b973a6fd20436c2fdaab621b92324
Eric Xu <utsxumiao@gmail.com>
2019-03-20 16:25:15 +1100

    Implemented Radio Binding function

--- AffoFly_Receiver.cpp
+++ AffoFly_Receiver.cpp
@@ -17,6 +17,7 @@ static const uint32_t signalTimeout = 500000; // 0.5s
 uint32_t token = 0;
 uint8_t channel = 0;
 ControlData controlData;
+TxBindData txBindData;
 
 RF24 radio(NRF_CE_PIN, NRF_CSN_PIN);
 
@@ -31,14 +32,20 @@ void setup() {
 
   pinMode(BIND_PIN, INPUT_PULLUP);
   if (digitalRead(BIND_PIN) == LOW) {
+#ifdef DEBUG
+    Serial.println("Binding...");
+#endif
     radioBind();
   } else {
     token = EEPROM_readToken();
     channel = EEPROM_readChannel();
     if (token == 0 || channel == 0) {
       // LED indicates binding is required
+#ifdef DEBUG
+      Serial.println("Binding required");
+#endif
       while (1) {
-
+        delay(600000);
       }
     } else {
       radioInit();
@@ -52,6 +59,7 @@ void setup() {
 void loop() {
   uint32_t currentTime = micros();
   receiveData(currentTime);
+  checkSignal(currentTime);
   Output_data(controlData);
 }
 
@@ -61,7 +69,7 @@ void radioInit() {
   radio.setAutoAck(false);
   radio.setChannel(channel);
   radio.setDataRate(RF24_250KBPS);
-  radio.openWritingPipe(0xE8E8F0F0E1LL);
+  radio.openReadingPipe(1, RADIO_PIPE);
   radio.startListening();
 #ifdef DEBUG
   Serial.print(F("Channel: ")); Serial.println(channel);
@@ -71,10 +79,13 @@ void radioInit() {
 }
 
 void receiveData(uint32_t currentTime) {
-  while(radio.available()) {
+  while (radio.available()) {
     radio.read(&controlData, sizeof(ControlData));
-    if(controlData.Token == token) {
+    if (controlData.Token == token) {
       previousSignalTime = currentTime;
+#ifdef DEBUG
+      Serial.print("AUX1: "); Serial.println(controlData.Aux1);
+#endif
     }
   }
 }
@@ -96,9 +107,56 @@ void radioBind() {
   radio.begin();
   radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_250KBPS);
-  radio.openWritingPipe(0xE8E8F0F0E1LL);
-  
+  radio.enableAckPayload();
+  radio.openReadingPipe(1, RADIO_PIPE);
+  radio.startListening();
+  delay(100);
+  radio.stopListening();
+  bool bound = false;
   while (1) {
-    uint32_t currentTime = micros();
+    if (!bound) {
+      TxBindData txBindData;
+      for (uint8_t i = RADIO_CHANNEL_LOWER_BOUNDARY; i <= RADIO_CHANNEL_UPPER_BOUNDARY; i++) {
+        radio.setChannel(i);
+        radio.startListening();
+        radio.writeAckPayload(1, RX_IDENTIFIER, 10);
+        delay(10);
+        while (radio.available()) {
+          radio.read(&txBindData, sizeof(TxBindData));
+          Serial.print("id: "); Serial.print(txBindData.TxIdentifier);  Serial.print("  ");
+          if (strcmp(txBindData.TxIdentifier, TX_IDENTIFIER) == 0) {
+            Serial.print("token: ");  Serial.print(txBindData.Token); Serial.println();
+            radio.writeAckPayload(1, RX_IDENTIFIER, strlen(RX_IDENTIFIER));
+            EEPROM_writeToken(txBindData.Token);
+            EEPROM_writeChannel(i);
+            channel = i;
+            bound = true;
+            break;
+          }
+        }
+        radio.stopListening();
+
+#ifdef DEBUG
+        Serial.print(".");
+#endif
+      }
+#ifdef DEBUG
+      Serial.println();
+#endif
+      pinMode(RED_LED_PIN, OUTPUT);
+      digitalWrite(RED_LED_PIN, HIGH);
+      delay(100);
+      digitalWrite(RED_LED_PIN, LOW);
+      delay(100);
+    } else {
+      pinMode(BLUE_LED_PIN, OUTPUT);
+      digitalWrite(BLUE_LED_PIN, HIGH);
+      delay(100);
+      digitalWrite(BLUE_LED_PIN, LOW);
+      delay(100);
+#ifdef DEBUG
+      Serial.print("Bound on channel: "); Serial.println(channel);
+#endif
+    }
   }
 }
